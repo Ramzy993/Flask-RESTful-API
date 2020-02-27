b0f46d6e38938d64bae9e0ff2f1502e9806db1d8
Ramzy993 <37620458+Ramzy993@users.noreply.github.com>
2020-02-27 17:26:17 +0200

    edit models

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'yousseframzy'
